27e9539378f883a3dfdfe9a8a89cf3f53a10265d
John <john.a.cowie@gmail.com>
2021-04-03 10:13:20 +0100

    First commit

---
+++ src/mudguard/core.clj
@@ -0,0 +1,6 @@
+(ns mudguard.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/mudguard/core_test.clj
@@ -0,0 +1,7 @@
+(ns mudguard.core-test
+  (:require [clojure.test :refer :all]
+            [mudguard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
